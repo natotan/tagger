4a3274e491e7bde2387366c341be6be14f4e47a6
Natasha <natashabs@gmail.com>
2014-07-03 09:25:25 -0500

    Created Tag class.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630230855) do
+ActiveRecord::Schema.define(version: 20140702231345) do
 
   create_table "contents", force: true do |t|
     t.string   "URL"
     t.text     "tags"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
