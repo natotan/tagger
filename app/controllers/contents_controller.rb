f73c6c45c04f8b811d1646a1499aac00a6943f54
Natasha <natashabs@gmail.com>
2014-07-03 18:36:44 -0500

    Created saveContent and SaveAddTag.

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -4,7 +4,21 @@ class ContentsController < ApplicationController
 	end
 	
 	def create
-		render plain: params[:content].inspect
-	end
+		@content = Content.new(content_params)
 		
+		@content.save
+		redirect_to @content
+			
+	end
+
+	def show
+		@content = Content.find(params[:id])
+	end
+	
+	private
+		def content_params
+			params.require(:content).permit(:URL, :tags)
+		end
+	
+	
 end
